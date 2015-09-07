44c9c7702658210e70bc5c80175c09be933ce9fc
jzajic <jan.zajic@corpus.cz>
2015-09-08 00:17:51 +0200

    fixed npe in PersistentStateStore

--- hbase-scheduler/src/main/java/org/apache/mesos/hbase/state/PersistentStateStore.java
+++ hbase-scheduler/src/main/java/org/apache/mesos/hbase/state/PersistentStateStore.java
@@ -59,7 +59,10 @@ public class PersistentStateStore implements IPersistentStateStore {
   public void setFrameworkId(Protos.FrameworkID id) {
 
     try {
-      hdfsStore.setRawValueForId(FRAMEWORK_ID_KEY, id.toByteArray());
+      if(id == null)
+        hdfsStore.setRawValueForId(FRAMEWORK_ID_KEY, new byte[]{});
+      else
+        hdfsStore.setRawValueForId(FRAMEWORK_ID_KEY, id.toByteArray());
     } catch (ExecutionException | InterruptedException e) {
       logger.error("Unable to set frameworkId", e);
       throw new PersistenceException(e);
